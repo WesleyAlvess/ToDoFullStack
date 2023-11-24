31b80955315d0e95e628d438110dc2671ff67469
WesleyAlvess <walvespereira96@gmail.com>
2023-11-24 13:20:28 -0300

    feast: index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const path = require("path")
 const routes = require("./routes/routes")
 const connectToDb = require("./database/db")
@@ -9,6 +9,7 @@ const connectToDb = require("./database/db")
 
 connectToDb()
 app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended: true}))
 app.use(routes)
